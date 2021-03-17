085d7d7970d4a56392d000b4477de743a111d6f6
Maarten Hoogendoorn <maarten@moretea.nl>
2021-03-17 21:32:42 +0100

    First iteration

---
+++ README.md
@@ -0,0 +1,44 @@
+# Vim clutch & push-to-talk.
+
+## Steps
+1. Find a Korg piano foot pedal
+2. Hook it up to an ESP-32
+3. Burn esphome to it
+4. Write a simple python script to shell out to `xdotool` to send the `i` to go to insert mode, and `Escape` to exit to command mode again. Also use `amixer` to (un)mute the microphone.
+5. Profit! (or at least, I had fun building this :))
+
+## Software
+- [ESPHome configuration](kludges.yaml)
+- [Python script](kludges.py)
+
+
+## Hardware
+
+The original module
+![](hw-pics/20210313_204116.jpg)
+
+The pedals are pushed up by strong springs
+![](hw-pics/20210314_193318.jpg)
+![](hw-pics/20210314_194012.jpg)
+
+This is the print containing all the interesting bits.
+There is a common source (I put 3v on it), and three lanes for the three pedals.
+Each pedal has three buttons that have less resistance if pushed harder.
+
+![](hw-pics/20210314_194553.jpg)
+![](hw-pics/20210314_194609.jpg)
+
+The first and second button (from back to front) of the pedal have both an 10k ohm resistor, replaced them with 150 ohm.
+The third one is connected directly to the lane for that pedal.
+![](hw-pics/20210314_194751.jpg)
+![](hw-pics/20210317_195439.jpg)
+
+I'm using ADC ports 3, 4 & 5.
+To let the voltage drain away after the pedal is pressed, I've connected the ports to ground with more 150 ohm resistors.
+![](hw-pics/20210317_195508.jpg)
+
+It fits quite well at the bottom of the pedal case. Had to break out some plastic to make room, but it fits quite well!
+![](hw-pics/20210317_201044.jpg)
+
+Demo video
+![](hw-pics/20210317_203102.mp4)
---
+++ kludge.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i python3 -p python3Packages.requests -p xdotool
+import requests, json
+import subprocess
+
+def transition(source, old, new):
+    # vim kludge
+    if source == "sensor-rechts":
+        if old is None:
+            return
+        if old == 0  and new > 0.1:
+            print("[vim] Insert")
+            subprocess.run("xdotool key 'i'", shell=True)
+        elif old > 0  and new == 0:
+            print("[vim] Exit")
+            subprocess.run("xdotool key 'Escape'", shell=True)
+    if source == "sensor-midden":
+        if old is None:
+            return
+        pass
+        # TODO
+    # push to talk 
+    if source == "sensor-links":
+        if old is None:
+            return
+        if old == 0  and new > 0.1:
+            print("[mic] Unmuting")
+            subprocess.run("amixer set Capture cap", shell=True)
+        elif old > 0  and new == 0:
+            print("[mic] Muting")
+            subprocess.run("amixer set Capture nocap", shell=True)
+
+url = "http://192.168.100.109/events"
+headers = {"Accept": "text/event-stream"}
+r = requests.get(url, headers=headers, stream=True)
+
+cmd = None
+raw_data = None
+states = {}
+
+for line in r.iter_lines():
+    line = line.decode("utf8")
+    if line == '':
+        continue
+
+    last_cmd = cmd
+    last_raw_data = raw_data
+    cmd, raw_data = line.split(":",1)
+    raw_data = raw_data.strip()
+    if last_cmd == "event" and last_raw_data == "state" and cmd == "data":
+        message = json.loads(raw_data)
+        source = message["id"]
+        state = message["value"]
+        old_state = states.get(message["id"], None)
+        if old_state != state:
+            transition(source, old_state, state)
+        states[source] = state
